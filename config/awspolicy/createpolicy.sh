a27816fb0ac474285795799cff82ef1426a36869
tschmidt <thorsten.schmidt@materna.de>
2018-04-06 09:07:30 +0200

    Add autoscaling node policy

---
+++ config/awspolicy/createpolicy.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+aws iam create-policy --policy-document file://asg-policy.json --policy-name ClusterAutoScaling
+aws iam attach-role-policy --role-name nodes.k8s-showcase.cluster.k8s.local --policy-arn arn:aws:iam::`aws sts get-caller-identity --output text --query 'Account'`:policy/ClusterAutoScaling
