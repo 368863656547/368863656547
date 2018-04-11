0abcf44db90bb99b0af79c2d080d0a6c28da638b
tschmidt <thorsten.schmidt@materna.de>
2018-04-11 10:21:44 +0200

    Fix workingdir for createpolicy.sh

--- config/awspolicy/createpolicy.sh
+++ config/awspolicy/createpolicy.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
+cd "${0%/*}"
 aws iam create-policy --policy-document file://asg-policy.json --policy-name ClusterAutoScaling
 aws iam attach-role-policy --role-name nodes.k8s-showcase.cluster.k8s.local --policy-arn arn:aws:iam::`aws sts get-caller-identity --output text --query 'Account'`:policy/ClusterAutoScaling
