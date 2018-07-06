21256f0f7fa4642fed8d15520d5780a6a36b6d8e
Arnd Kleinbeck <arnd.kleinbeck@materna.de>
2018-07-06 10:42:24 +0200

    Test

--- app/app/react-app/src/components/Header/index.js
+++ app/app/react-app/src/components/Header/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './styles.css'
 
 const Header = () => {
-  const title = 'Welcome to the atsea shop'
+  const title = 'Welc#### to the atsea shop'
   const subtitle = ''
   return (
     <div className='headerSection'>
