1fa209f688398bc3351a07716ed12184a1e56559
hdreier <Hans-Dieter.Dreier@mterna.de>
2018-07-09 14:23:21 +0200

    sql script

--- app/windows/dotnet/Docker.AtSea/Docker.AtSea.Database/dbo/Tables/Products.sql
+++ app/windows/dotnet/Docker.AtSea/Docker.AtSea.Database/dbo/Tables/Products.sql
@@ -1,8 +1,8 @@
 ﻿CREATE TABLE [dbo].[Products]
 (
 	[ProductId] BIGINT NOT NULL PRIMARY KEY IDENTITY, 
-    [Description] VARCHAR(MAX) NOT NULL, 
-    [Image] VARBINARY(MAX) NULL, 
+    [Description] VARCHAR(255) NOT NULL, 
+    [Image] VARBINARY(65536) NULL, 
     [Price] NUMERIC(13, 2) NOT NULL, 
     [Name] VARCHAR(255) NOT NULL
 )
