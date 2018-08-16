e82f9f7304ed4c1aaddca4366a2b6cc467caae21
Arnd Kleinbeck <arnd.kleinbeck@materna.de>
2018-08-16 22:21:01 +0200

    Neuer Name

--- app/app/react-app/src/components/Header/index.js
+++ app/app/react-app/src/components/Header/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './styles.css'
 
 const Header = () => {
-  const title = 'Welcome to the atsea shop'
+  const title = 'Welcome to the pleasure dome'
   const subtitle = ''
   return (
     <div className='headerSection'>
